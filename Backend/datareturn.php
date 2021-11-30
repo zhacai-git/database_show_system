a5c4c62c3302b3a1d2f80ceaa70425420f0757c5
zhacai <lzxncut@mail.ncut.edu.cn>
2021-11-30 09:57:08 +0800

    File Path Changed

---
+++ Backend/datareturn.php
@@ -0,0 +1,60 @@
+<?php
+header('Content-Type:application/json'); // 返回头类型定义
+date_default_timezone_set('Asia/Shanghai'); // php时区修正
+$time = date("Y/m/d G:i:s");
+$servername = "localhost"; // 服务器地址
+$username = "root"; // 用户名
+$password = "YourPassWordHere"; // 密码
+$dbName = "YourDBNameHere"; // 数据库名
+$conn = mysqli_connect($servername,$username,$password,$dbName); // 数据库连接
+if (!$conn) {
+    die("连接失败！".mysqli_connect_error());
+}
+$type = $_GET['type'];
+$sql = $_GET['sql']; // url get查询参数提取
+switch ($type) {
+    case 1: {
+        $counter = 1;
+        $return = array();
+        $returnField = array();
+        if ($sql==='') {
+            $return['status'] = array("code"=>"1001","success"=>false,"message"=>"请输入查询语句！");
+        } else {
+            $result = mysqli_query($conn,$sql);
+            if ($result) {
+                $return['status'] = array("code"=>"1000","success"=>true,"message"=>"成功","query_time"=>$time,"sql_performed"=>$sql);
+                $lines = mysqli_num_rows($result);
+                $return['field'] = array();
+                $return['content'] = array();
+                if ($lines>0) {
+                    while ($field = mysqli_fetch_field($result)) {
+                        array_push($returnField,$field->name);
+                    }
+                    $return['field'] = $returnField;
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $row['index']=$counter;
+                        array_push($return['content'],$row);
+                        $counter++;
+                    }
+                }
+            } else {
+                $return['status'] = array("code"=>"1002","success"=>false,"message"=>mysqli_error($conn));
+            }
+        }
+        break;
+    }
+    case 2: {
+        if ($sql==='') {
+            $return['status'] = array("code"=>"1001","success"=>false,"message"=>"请输入SQL语句！");
+        } else {
+            $result = mysqli_query($conn,$sql);
+            if ($result) {
+                $return['status'] = array("code"=>"1000","success"=>true,"message"=>"执行成功！","sql_performed"=>$sql);
+            } else {
+                $return['status'] = array("code"=>"1002","success"=>false,"message"=>mysqli_error($conn));
+            }
+        }
+    }
+}
+echo json_encode($return);
+mysqli_close($conn);
---
+++ Frontend/src/views/AppMain.vue
@@ -0,0 +1,69 @@
+<template>
+  <el-container class="main-container-wrapper">
+    <el-header style="height: 70px">
+      <h2 class="header_text">数据库原理展示页面</h2>
+    </el-header>
+    <el-container>
+      <el-aside style="width: 200px;height: 1100px">
+        <el-menu
+          :default-active="$route.name"
+          :router="true"
+        >
+          <el-menu-item index="standard_demo" :route="{path: '/standard_demo'}">
+            <span>标准语句演示</span>
+          </el-menu-item>
+          <el-menu-item index="sql_custom_query" :route="{path: '/sql_custom_query'}">
+            <span>SQL自定义查询</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "AppMain",
+  mounted() {
+    this.$router.push('/standard_demo')
+  }
+}
+</script>
+
+<style scoped>
+.main-container-wrapper {
+  border-radius: 10px 10px 10px 10px;
+  box-shadow: 0 0 40px rgb(0 0 0 / 10%);
+  height: 100%;
+}
+
+.header_text {
+  position: relative;
+  float: left;
+}
+
+.el-footer {
+  background-color: #b3c0d1;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 200px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-main {
+
+}
+</style>
